f6f5428d2085be62686a4a2490378386ac949988
2tae9yu <2tae9yu@naver.com>
2024-03-22 12:52:48 +0900

    3월 22일 강의
    예제 2-1

---
+++ Chapter21.java
@@ -0,0 +1,10 @@
+// 예제 2-1
+
+public class Chapter21 {
+    public static void main(String[] args) {
+        var foo = 200;
+        var name = "kitae";
+
+        System.out.println(foo + name);
+    }
+}
