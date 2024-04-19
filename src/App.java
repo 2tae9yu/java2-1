9de9038de7d9eaf29d4f04e6243669182009e3fd
2tae9yu <2tae9yu@naver.com>
2024-04-19 11:19:47 +0900

    java2.add, java3 패키지 import

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java2.add.*;
+import java3.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Add foo = new Add();
+        System.out.println(foo.add(10, 20));
+
+        Calc2 bar = new Calc2();
+        System.out.println(bar.add2(30, 20));
     }
 }
